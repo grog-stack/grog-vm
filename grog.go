4ce8eea3bd42ebe0ad9aa7b7b0431c16100d690f
Martin Straus <martin.straus@fit.com.ar>
2020-06-02 00:25:29 -0300

    Added machine initialization with registers and memory.

--- grog.go
+++ grog.go
@@ -1,7 +1,25 @@
 package main
 
 import "fmt"
+import "loader"
+
+type Machine struct {
+	Name      string
+	Registers [16]Register
+	Memory    []int8
+}
+
+func (m *Machine) Explain() {
+	fmt.Printf("Machine: %s\n", m.Name)
+	fmt.Printf("Memory size: %d bytes\n", len(m.Memory))
+}
+
+type Register struct {
+	Name  string
+	Value int16
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	machine := Machine{Name: "Grog", Memory: make([]int8, 32*1024)}
+	machine.Explain()
 }
