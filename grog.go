99eb65e2fc78c70a182282b55ae85f455f93a68b
Martin Straus <martin.straus@fit.com.ar>
2020-07-18 12:00:47 -0300

    Added registers, program counter, and sample machine running.

--- grog.go
+++ grog.go
@@ -1,17 +1,29 @@
 package main
 
 import "fmt"
-import "loader"
+import "io/ioutil"
+import "os"
 
 type Machine struct {
-	Name      string
-	Registers [16]Register
-	Memory    []int8
+	Name           string
+	Registers      [16]Register
+	Memory         []byte
+	ProgramCounter int32
 }
 
 func (m *Machine) Explain() {
 	fmt.Printf("Machine: %s\n", m.Name)
 	fmt.Printf("Memory size: %d bytes\n", len(m.Memory))
+	fmt.Printf("Program counter: %d\n", m.ProgramCounter)
+}
+
+func (m *Machine) Run() {
+	fmt.Println("Running...")
+	fmt.Println("Finished!")
+}
+
+func (m *Machine) Load(memory []byte) {
+
 }
 
 type Register struct {
@@ -20,6 +32,21 @@ type Register struct {
 }
 
 func main() {
-	machine := Machine{Name: "Grog", Memory: make([]int8, 32*1024)}
+	machine := Machine{Name: "Grog", Memory: make([]byte, 32*1024)}
+	memory := readOrPanic(os.Args[0])
 	machine.Explain()
+	machine.Load(memory)
+	machine.Run()
+}
+
+func readOrPanic(filename string) (data []byte) {
+	data, err := ioutil.ReadFile(os.Args[0])
+	check(err)
+	return
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
